40df99ba5a84c0f46336778881e8140b4db8dab2
guilhermemoriggidesouza <guilhermemoriggidesouza@gmail.com>
2024-04-08 18:59:21 -0300

    fix: code

--- src/api.ts
+++ src/api.ts
@@ -2,68 +2,65 @@ import crypto from "crypto";
 import express from "express";
 import pgp from "pg-promise";
 import { validate } from "./validateCpf";
+
 const app = express();
 app.use(express.json());
+app.listen(3000);
 
 app.post("/signup", async function (req, res) {
-	let result;
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+	const payload = req.body
+	const connectionDB = connectInDb();
 	try {
-		const id = crypto.randomUUID();
-
-		const [acc] = await connection.query("select * from cccat15.account where email = $1", [req.body.email]);
-		if (!acc) {
-
-			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (req.body.email.match(/^(.+)@(.+)$/)) {
-
-					if (validate(req.body.cpf)) {
-						if (req.body.isDriver) {
-							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
-
-			} else {
-				// invalid name
-				result = -3;
-			}
-
-		} else {
-			// already exists
-			result = -4;
+		const accountByEmail = await getAccountByEmail(connectionDB, req.body.email)
+		if (accountByEmail) {
+			throw new Error("Account already exists");
 		}
-		if (typeof result === "number") {
-			res.status(422).send(result + "");
-		} else {
-			res.json(result);
-		}
+		await validatePayload(req.body)
+		const createdAccount = await createAccount(connectionDB, payload)
+		res.json(createdAccount);
+	} catch (error: any) {
+		return res.status(422).send(error.message);
 	} finally {
-		await connection.$pool.end();
+		await closeConnectionDB(connectionDB)
 	}
 });
 
-app.listen(3000);
+const connectInDb = () => pgp()("postgres://postgres:123456@localhost:5432/app")
+
+const getAccountByEmail = async (connectionDB: any, email: string) => {
+	return await connectionDB.query("select * from cccat15.account where email = $1", [email]);
+}
+
+const createAccount = async (connectionDB: any, payload: Payload) => {
+	const id = crypto.randomUUID();
+	await connectionDB.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, payload.name, payload.email, payload.cpf, payload.carPlate, !!payload.isPassenger, !!payload.isDriver]);
+	const createdAccount = {
+		accountId: id
+	};
+	return createdAccount
+}
+
+const validatePayload = async (payload: Payload) => {
+	const validateName = (name: string) => name.match(/[a-zA-Z] [a-zA-Z]+/)
+	const validateEmail = (email: string) => email.match(/^(.+)@(.+)$/)
+	const validateCarPlate = (carPlate: string) => carPlate.match(/[A-Z]{3}[0-9]{4}/)
+
+	if (!validateName(payload.name)) {
+		throw new Error("Wrong name");
+	}
+	if (!validateEmail(payload.email)) {
+		throw new Error("Wrong email");
+	}
+	if (validate(payload.cpf)) {
+		throw new Error("Wrong cpf");
+	}
+	if (payload.isDriver && payload.carPlate && !validateCarPlate(payload.carPlate)) {
+		throw new Error("Wrong carPlate");
+	}
+}
+
+const closeConnectionDB = async (connectionDB: any) => {
+	await connectionDB.$pool.end();
+}
+
+type Payload = { name: string; email: string; cpf: string; isDriver?: any; carPlate?: string; isPassenger?: boolean }
--- test/api.test.ts
+++ test/api.test.ts
@@ -14,3 +14,72 @@ test("Deve criar uma conta para o passageiro", async function () {
 	const output = await axios.post("http://localhost:3000/signup", input);
 	console.log(output.status, output.data);
 });
+
+test("Deve criar uma conta para motorista", async () => {
+	const input = {
+		name: "John Doe",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		isDriver: true,
+		carPlate: 'abc1234'
+	};
+	const output = await axios.post("http://localhost:3000/signup", input);
+	console.log(output.status, output.data);
+})
+
+test("Deve retornar erro quando a placa estiver errada", async () => {
+	const input = {
+		name: "John Doe",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		isDriver: true,
+		carPlate: 'ac1234'
+	};
+	const output = await axios.post("http://localhost:3000/signup", input);
+	console.log(output.status, output.data);
+})
+
+test("Deve retornar erro quando o cpf estiver errado", async () => {
+	const input = {
+		name: "John Doe",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "877448800",
+		isPassenger: true
+	};
+	const output = await axios.post("http://localhost:3000/signup", input);
+	console.log(output.status, output.data);
+})
+
+test("Deve retornar erro quando o nome estiver errado", async () => {
+	const input = {
+		name: "John Do21e",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		isPassenger: true
+	};
+	const output = await axios.post("http://localhost:3000/signup", input);
+	console.log(output.status, output.data);
+})
+
+test("Deve retornar erro quando o email estiver errado", async () => {
+	const input = {
+		name: "John Doe",
+		email: `john.doe${Math.random()}gmail.com`,
+		cpf: "87748248800",
+		isPassenger: true
+	};
+	const output = await axios.post("http://localhost:3000/signup", input);
+	console.log(output.status, output.data);
+})
+
+test("Deve retornar erro se já exstir user", async () => {
+	const input = {
+		name: "John Doe",
+		email: `john.doe@gmail.com`,
+		cpf: "87748248800",
+		isPassenger: true
+	};
+	const output = await axios.post("http://localhost:3000/signup", input);
+	console.log(output.status, output.data);
+})
+
